e85c98183a91e817bfdfa4a1a104eb2116ddd8f6
Hoxtygen <hoxtygen@live.com>
2019-07-12 10:44:53 +0100

    Get action by Id

--- action/actionRouter.js
+++ action/actionRouter.js
@@ -13,4 +13,37 @@ router.get('/', async(req, res) => {
         })
     }
 })
+
+router.get('/:id', validateActionId, async(req, res) => {
+    res.status(200).send(req.action);
+})
+
+
+
+
+
+//middleware
+async function validateActionId(req, res, next) {
+    const id = req.params.id;
+    if (Number.isNaN(id) || id % 1 !== 0 || id < 0) {
+        return res.status(400).json({
+            errorMessage: "Invalid action id supplied"
+        });
+    }
+    try {
+        const action = await actionModel.get(id);
+        if (!id) {
+            res.status(404).json({
+                errorMessage: "The action with the specified ID does not exist."
+            })
+        }
+        req.action = action;
+    } catch (error) {
+        return res.status(500).json({
+            error
+        })
+    }
+    return next();
+};
+
 module.exports = router;
